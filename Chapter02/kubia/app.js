a12c3a23b7a13bf157ae31705ceb5b60a058b2d3
suma160896 <159858190+suma160896@users.noreply.github.com>
2024-06-02 20:43:49 -0400

    Update app.js

--- Chapter02/kubia/app.js
+++ Chapter02/kubia/app.js
@@ -3,12 +3,29 @@ const os = require('os');
 
 console.log("Kubia server starting...");
 
-var handler = function(request, response) {
-  console.log("Received request from " + request.connection.remoteAddress);
+// Handler function for port 8080
+var handler8080 = function(request, response) {
+  console.log("Received request on port 8080 from " + request.connection.remoteAddress);
   response.writeHead(200);
-  response.end("You've hit the container " + os.hostname() + "\n");
+  response.end("You've hit port 8080 on " + os.hostname() + "\n");
 };
 
-var www = http.createServer(handler);
-www.listen(8080);
+// Handler function for port 8081
+var handler8081 = function(request, response) {
+  console.log("Received request on port 8081 from " + request.connection.remoteAddress);
+  response.writeHead(200);
+  response.end("You've hit port 8081 on " + os.hostname() + "\n");
+};
 
+// Create HTTP server instances
+var server8080 = http.createServer(handler8080);
+var server8081 = http.createServer(handler8081);
+
+// Listen on ports 8080 and 8081
+server8080.listen(8080, function() {
+  console.log("Server listening on port 8080");
+});
+
+server8081.listen(8081, function() {
+  console.log("Server listening on port 8081");
+});
